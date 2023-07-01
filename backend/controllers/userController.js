a3331112ac4e44223a775e8f8aa590293c29db97
daggim99 <programmerdaggim99@gmail.com>
2023-07-01 12:11:02 +0300

    my second commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -254,7 +254,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
   }).save();
 
   // Construct Reset Url
-  const resetUrl = `http://localhost:4000/resetpassword/${resetToken}`;
+  const resetUrl = `https://tms-mm9y.onrender.com:7000/resetpassword/${resetToken}`;
 
   // Reset Email
   const message = `
