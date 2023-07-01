eb5f8ce94ec8710b2e13cee3294e40159e140402
daggim99 <programmerdaggim99@gmail.com>
2023-07-01 12:43:44 +0300

    my forth commit

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://tms-mm9y.onrender.com"],
     credentials: true,
   })
 );
